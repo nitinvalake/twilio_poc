ab0452d2c10e358e5d08801b683039cfed852082
Valake <nitin.valake@hp.com>
2020-01-28 18:23:41 +0530

    Corrected formatting for request

--- Twilio_poc_main.go
+++ Twilio_poc_main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strings"
 	"net/http"
 	"net/url"
-	"encoding/json"
+	"os"
+	"strings"
+
 	//"io/ioutil"
 	"log"
+
 	"github.com/gorilla/mux"
 )
 
@@ -30,22 +33,22 @@ func sendSms(w http.ResponseWriter, r *http.Request) {
 	//Twilio credentials
 	accountSid := "AC0e9c4ba71a13276c2d05018fb452fff6"
 	authToken := "c248ed0ab16317542bf12475058fc8e9"
-	urlStr := "https://api.twilio.com/2010-04-01/Accounts/"+accountSid+"/Messages.json"
-	fromPhoneNumber:= "+17867892028"
+	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+	fromPhoneNumber := "+17867892028"
 
 	var smsReq sendMessageRequest
 	err := json.NewDecoder(r.Body).Decode(&smsReq)
-	
+
 	if err != nil {
 		fmt.Fprintf(w, "Kindly enter valid data to send sms")
 	}
-	fmt.Println("Client Request: %+v", smsReq)
+	fmt.Printf("Client Request: %+v", smsReq)
 
 	//Fetching sms data from sms send request
 	msgData := url.Values{}
- 	msgData.Set("To",smsReq.To)
-	msgData.Set("From",fromPhoneNumber)
-	msgData.Set("Body",smsReq.Message)
+	msgData.Set("To", smsReq.To)
+	msgData.Set("From", fromPhoneNumber)
+	msgData.Set("Body", smsReq.Message)
 	msgDataReader := *strings.NewReader(msgData.Encode())
 
 	client := &http.Client{}
@@ -53,30 +56,32 @@ func sendSms(w http.ResponseWriter, r *http.Request) {
 	req.SetBasicAuth(accountSid, authToken)
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	fmt.Println("Request to be sent to Twilio: ",req)
+	fmt.Println("Request to be sent to Twilio: ", req)
 	var resp1 response
 
 	resp, _ := client.Do(req)
-	fmt.Println("Twilio Response: ",resp)
-	if (resp.StatusCode >= 200 && resp.StatusCode < 300) {
+	fmt.Println("Twilio Response: ", resp)
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		var data map[string]interface{}
 		decoder := json.NewDecoder(resp.Body)
 		err := decoder.Decode(&data)
-		if (err == nil) {
+		if err == nil {
 			fmt.Println(data["sid"])
 			resp1 = response{Status: "Success", StatusText: "SMS Sent!"}
 			w.WriteHeader(http.StatusOK)
 		}
 	} else {
-		fmt.Println(resp.Status);
-		resp1 = response{Status: "Failed", StatusText: resp.Status}	
-		w.WriteHeader(resp.StatusCode)	
-	}	
+		fmt.Println(resp.Status)
+		resp1 = response{Status: "Failed", StatusText: resp.Status}
+		w.WriteHeader(resp.StatusCode)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp1)
 }
 
 func main() {
+	dbPass := os.Getenv("DATABASE_PASS")
+	fmt.Println("ENV: ", dbPass)
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/sendsms", sendSms).Methods("POST")
